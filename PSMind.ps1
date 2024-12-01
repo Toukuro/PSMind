953bc8b7454e3575295f7e21ab2ed731eb1c25d6
Toukuro <kj_adachi@nifty.com>
2024-12-01 09:10:25 +0900

    #12 メインモジュールでメインモデルを生成

--- PSMind.ps1
+++ PSMind.ps1
@@ -1,19 +1,19 @@
 using module PSLogger
+using module ".\Models\PSMindModel.psm1"
 using module ".\Forms\PSMindForm.psm1"
-
-# set version no.
-[String] $version = "0.1"
 
 # get logger and set loglevel
 [PSLogger] $logger = [PSLogger]::GetLogger()
 $logger.LogLevel = [PSLogLevel]::Debug
 
 # output start message
-$logger.WriteInfo("PSMind, Version $version Started.")
+$logger.WriteInfo("PSMind Started.")
 
 # todo: create PSMindModel
+$model = [PSMindModel]::new()
+$logger.WriteInfo("  current version $($model.Version)")
 # todo: create PSMindForm
-$form = [PSMindForm]::new($null)
+$form = [PSMindForm]::new($model)
 
 # todo: show PSMindForm
 $form.Form.ShowDialog()
